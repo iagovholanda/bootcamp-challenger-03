6cee31e19e8acde5b5516803aaf8ff85fc25fa22
Iago Holanda <iagogholanda@gmail.com>
2020-07-26 18:44:39 -0300

    Final Challenger

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  /* Valores dos repositorios e seu estado. */
+  const [repositories, setRepositories] = useState([])
+
+  /* Disparo de funções e retorno de infomações em tela. */
+  useEffect(() => {
+    Api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await Api.post('repositories', {
+      title: "Bootcamp - Challenger Teste",
+      url: "http://www.github.com/bootcamp-challenger-teste",
+      techs: ["React", "NodeJs"]
+    })
+
+    const repositorie = response.data
+
+    /* 
+      setRepositories -> Alterando o estado.
+      repositorie -> Recebenmdo o novo valor.
+    */
+    setRepositories([...repositories, repositorie])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await Api.delete(`repositories/${id}`)
+
+    /* 
+      Mantendo apenas os repositorios que possui id diferente do qual
+      foi removido. 
+    */
+    setRepositories(repositories.filter(
+      repositorie => repositorie.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie => (
+          <li key={repositorie.id}>
+            {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
